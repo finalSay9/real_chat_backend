751952db1ce51e5d14fa72681023c911195933af
finalSay9 <chimwazaevan1@gmail.com>
2026-03-06 14:28:15 +0200

    add some dependecies

--- routes/websockets.py
+++ routes/websockets.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI, Websocket,WebsocketDisconnect,Depends,Query,APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, insert
+from sqlalchemy.orm import selectinload
+import json
+import logging
+from database import get_db, SessionLocal
+from models import User, Room, Message, room_members, UserStatus
+from schemas import WSIncoming
+from auth import decode_token
+from ws_manager import manager
 
 
 
